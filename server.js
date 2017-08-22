90a59df0b3b7043a02a5da240fbf3032df2bdcaa
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-08-22 07:40:01 -0500

    scrape completed

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var request = require("request");
 var logger = require("morgan");
 var cheerio = require("cheerio");
 
-
+var Article = require("./models/articles.js");
 
 mongoose.Promise = Promise;
 
@@ -29,26 +30,49 @@ db.once("open", function() {
     console.log("Mongoose connection successful.");
 });
 
+app.get("/scrape", function(req, res) {
 
 request("https://www.nytimes.com/section/technology?WT.nav=page&action=click&contentCollection=Tech&module=HPMiniNav&pgtype=Homepage&region=TopBar", function(error, response, html) {
     var $ = cheerio.load(html);
-    var results = [];
     $("h2.headline").each(function(i, element) {
-        var title = $(element).text();
-        var link = $(element).children().attr("href");
 
-        $("p.summary").each(function(i, element) {
-            var summary = $(element).text();
+        var result = {};
 
-            results.push({
-                title: title,
-                link: link,
-                summary: summary
+        result.title = $(element).text();
+        result.link = $(element).children().attr("href");
+        result.summary = $(element).next().text();
+
+            var entry = new Article(result);
+        
+            entry.save(function(err, doc) {
+                // Log any errors
+                if (err) {
+                console.log(err);
+                }
+                // Or log the doc
+                else {
+                console.log(doc);
+                }
             });
         });
     });
-    console.log(results);
 });
+
+app.get("/articles", function(req, res) {
+  // Grab every doc in the Articles array
+  Article.find({}, function(error, doc) {
+    // Log any errors
+    if (error) {
+      console.log(error);
+    }
+    // Or send the doc to the browser as a json object
+    else {
+      res.json(doc);
+    }
+  });
+});
+
+
 
 
 app.listen(3080, function() {
