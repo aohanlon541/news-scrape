b2e154732abd7b10963760327185f0bafa262bc6
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-08-26 19:14:13 -0500

    mostly done

--- models/notes.js
+++ models/notes.js
@@ -2,9 +2,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var NoteSchema = new Schema({
-  title: {
-    type: String
-  },
+
   body: {
     type: String
   }
--- public/app.js
+++ public/app.js
@@ -1,11 +1,23 @@
 $.getJSON("/articles", function(data) {
   for (var i = 0; i < data.length; i++) {
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append(
+        "<div class='article'> <h2 data-id='" + data[i]._id + "'>" + data[i].title + "</h2>" +
+        "<a href='" + data[i].link + "' class='articleLink'>Read article</a>" +
+        "</div>");
   }
 });
 
+$("#date").append(moment().format("MMMM Do YYYY"));
 
-$(document).on("click", "p", function() {
+$('.composeNote').click(function() {
+  $('#notes').animate({
+    height: 'toggle'
+  }, 1500, function() {
+  });
+});
+
+
+$(document).on("click", "h2", function() {
   $("#notes").empty();
   var thisId = $(this).attr("data-id");
 
@@ -15,13 +27,10 @@ $(document).on("click", "p", function() {
   })
     .done(function(data) {
       console.log(data);
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      $("#notes").append("<input id='titleinput' name='title' >");
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
 
       if (data.note) {
-        $("#titleinput").val(data.note.title);
         $("#bodyinput").val(data.note.body);
       }
     });
